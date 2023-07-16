8df13431142455f5cddbf8582667da968551334a
Valentin Brosseau <c4software@gmail.com>
2023-07-16 15:12:41 +0200

    Correction script

--- startup.bat
+++ startup.bat
@@ -1,20 +1,22 @@
 @echo off
+Setlocal EnableDelayedExpansion
 
 rem Set the compose file and profile
 set COMPOSE_FILE="./docker-compose.yml"
-set PROFILE="--profile default"
+set PROFILE=--profile default
 
 rem Ask the user if they want to start mongo
-echo "Do you want to start mongo? (y/N)"
+echo Voulez-vous demarrer MongoDB? (y/N)
 set /p mongo=""
 if "%mongo%" == "y" (
-  set PROFILE=!--profile mongo %PROFILE%!
+  set PROFILE=--profile mongo !PROFILE!
+)
 
 rem Ask the user if they want to start MailHog
-echo "Do you want to start MailHog? (y/N)"
+echo Voulez-vous demarrer MailHog? (y/N)
 set /p mailhog=""
 if "%mailhog%" == "y" (
-  set PROFILE=!--profile mailhog %PROFILE%!
+  set PROFILE=--profile mailhog %PROFILE%
 )
 
 rem Set environment variables
@@ -27,27 +29,26 @@ set DAMP_MAIL_PORT_WEB=8025
 set DAMP_HOME_DIRECTORY=./
 
 rem Stop containers
-docker-compose -f "%COMPOSE_FILE%" "%PROFILE%" down
+docker compose -f %COMPOSE_FILE% %PROFILE% down
 
 rem Start containers
-docker compose -f "%COMPOSE_FILE%" "%PROFILE%" up -d
+docker compose -f %COMPOSE_FILE% %PROFILE% up -d
 
-cls
 
 rem Print the start message
 echo DAMP is started
-echo MySQL : localhost:%MYSQL_PORT%
-echo HTTP / PHP : http://localhost:%HTTP_PORT%
-echo PhpMyAdmin : http://localhost:%PMA_PORT%
+echo MySQL : localhost:%DAMP_DB_PORT%
+echo HTTP / PHP : http://localhost:%DAMP_WEB_PORT%
+echo PhpMyAdmin : http://localhost:%DAMP_PMA_PORT%
 if "%mongo%" == "y" (
-  echo "MongoDB : localhost:%MONGO_PORT%"
+  echo MongoDB : localhost:%DAMP_MONGO_PORT%
 )
 if "%mailhog%" == "y" (
-  echo "MailHog : http://localhost:%MAIL_PORT_WEB% / smtp://localhost:%MAIL_PORT%"
+  echo MailHog : http://localhost:%DAMP_MAIL_PORT% / smtp://localhost:%DAMP_MAIL_PORT_WEB%
 )
 
 rem Wait for user input
 pause
 
 rem Stop containers
-docker compose -f "%COMPOSE_FILE%" "%PROFILE%" down
+docker compose -f %COMPOSE_FILE% %PROFILE% down
